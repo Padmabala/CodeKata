6ffae67fac352e0b1989b72288e483ed59a87045
kpadmabala@gmail.com <kpadmabala@gmail.com>
2019-10-26 09:59:49 +0530

    Alterate Sorted Array elements

--- AlternateSortOrderArray.py
+++ AlternateSortOrderArray.py
@@ -1,1 +1,29 @@
-print("hello world")
+
+def sort(arr,low,high):
+    if(low<high):
+        pivot=partition(arr,low,high)
+        sort(arr,low,pivot-1)
+        sort(arr,pivot+1,high)
+def partition(arr,low,high):
+    pivot=arr[high]
+    i=low-1
+    for j in range(low,high):
+        if(arr[j]<pivot):
+            i=i+1
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
+def alternateArray(arr,n):
+    sort(arr,0,n)
+n=int(input())
+arr=list(map(int,input().split()[:n]))
+alternateArray(arr,len(arr)-1)
+start=0
+end=len(arr)-1
+while 1:
+    print(arr[start])
+    start+=1
+    if (start >=end):
+        break
+    print(arr[end])
+    end-=1
